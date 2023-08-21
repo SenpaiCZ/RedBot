ad07b459661288f6bceecb6e9de18cafa847ed05
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-21 17:48:33 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -16,7 +16,7 @@ class Roll(commands.Cog):
             
         if dice_type not in [4, 6, 8, 10, 12, 20, 100]:
             embed = discord.Embed(
-                title="Invalid Dice Type",
+                title="Špatný formát kostek.",
                 description="Use :game_die: D4, D6, D8, D10, D12, D20, or D100.",
                 color=discord.Color.red()
             )
@@ -32,5 +32,27 @@ class Roll(commands.Cog):
             description=f"Rolls: {rolls_str}\nTotal: {total}",
             color=discord.Color.green()
         )
+        
+        await ctx.send(embed=embed)
+
+    @commands.command(name="cthulhu")  # Přidáváme nový příkaz "cthulhu"
+    async def cthulhu_stats(self, ctx):
+        embed = discord.Embed(
+            title="Call of Cthulhu Stats",
+            description="Basic statistics for a Call of Cthulhu character:",
+            color=discord.Color.blue()
+        )
+        embed.add_field(name="Strength (STR) :muscle:", value="70", inline=True)
+        embed.add_field(name="Dexterity (DEX) :runner:", value="50", inline=True)
+        embed.add_field(name="Constitution (CON) :heart:", value="60", inline=True)
+        embed.add_field(name="Intelligence (INT) :brain:", value="80", inline=True)
+        embed.add_field(name="Power (POW) :zap:", value="50", inline=True)
+        embed.add_field(name="Charisma (CHA) :sparkles:", value="40", inline=True)
+        embed.add_field(name="Education (EDU) :mortar_board:", value="70", inline=True)
+        embed.add_field(name="Size (SIZ) :bust_in_silhouette:", value="60", inline=True)
+        embed.add_field(name="Hit Points :heartpulse:", value="12", inline=True)
+        embed.add_field(name="Magic Points :sparkles:", value="10", inline=True)
+        embed.add_field(name="Luck :four_leaf_clover:", value="45", inline=True)
+        embed.add_field(name="Sanity :scales:", value="70", inline=True)
         
         await ctx.send(embed=embed)

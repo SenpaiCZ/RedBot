fdfd1116676daac4910be6ec76922e94e4b08b06
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-21 17:50:03 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -6,9 +6,9 @@ class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def dr(self, ctx, dice_expression):
- if 'd' in dice_expression:
+    @commands.command(name="dr")  # Změňte jméno příkazu zde
+    async def dice_roll(self, ctx, dice_expression):
+        if 'd' in dice_expression:
             num_dice, dice_type = map(int, dice_expression.lower().split('d'))
         else:
             num_dice = 1  # Předpokládáme 1 kostku, pokud první číslo není zadáno
@@ -16,7 +16,7 @@ class Roll(commands.Cog):
             
         if dice_type not in [4, 6, 8, 10, 12, 20, 100]:
             embed = discord.Embed(
-                title="Špatný formát kostek.",
+                title="Invalid Dice Type",
                 description="Use :game_die: D4, D6, D8, D10, D12, D20, or D100.",
                 color=discord.Color.red()
             )

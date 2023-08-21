e3932bd94401de06e5fd78b3a2a4cc96198ac017
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-21 17:53:47 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -8,16 +8,20 @@ class Roll(commands.Cog):
 
     @commands.command()
     async def dr(self, ctx, dice_expression):
-        if 'd' in dice_expression:
+        try:
             num_dice, dice_type = map(int, dice_expression.lower().split('d'))
-        else:
-            num_dice = 1  # Předpokládáme 1 kostku, pokud první číslo není zadáno
-            dice_type = int(dice_expression)
-            
-        if dice_type not in [4, 6, 8, 10, 12, 20, 100]:
+            if dice_type not in [4, 6, 8, 10, 12, 20, 100]:
+                embed = discord.Embed(
+                    title="Invalid Dice Type",
+                    description="Use :game_die: D4, D6, D8, D10, D12, D20, or D100.",
+                    color=discord.Color.red()
+                )
+                await ctx.send(embed=embed)
+                return
+        except ValueError:
             embed = discord.Embed(
-                title="Invalid Dice Type",
-                description="Use :game_die: D4, D6, D8, D10, D12, D20, or D100.",
+                title="Invalid Dice Expression",
+                description="Use format XdY where X is the number of dice and Y is the dice type.",
                 color=discord.Color.red()
             )
             await ctx.send(embed=embed)

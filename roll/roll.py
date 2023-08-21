1a3fd3727f9d1e9e4b2b5a1a2a92e3608e3e8c8f
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-21 18:59:35 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -51,28 +51,6 @@ class Roll(commands.Cog):
         )
         
         await ctx.send(embed=embed)
-
-    @commands.command()
-    async def investigator(self, ctx):
-        embed = discord.Embed(
-            title="Call of Cthulhu Stats",
-            description="Basic statistics for a Call of Cthulhu character:",
-            color=discord.Color.blue()
-        )
-        embed.add_field(name="Strength (STR) :muscle:", value="70", inline=True)
-        embed.add_field(name="Dexterity (DEX) :runner:", value="50", inline=True)
-        embed.add_field(name="Constitution (CON) :heart:", value="60", inline=True)
-        embed.add_field(name="Intelligence (INT) :brain:", value="80", inline=True)
-        embed.add_field(name="Power (POW) :zap:", value="50", inline=True)
-        embed.add_field(name="Charisma (CHA) :sparkles:", value="40", inline=True)
-        embed.add_field(name="Education (EDU) :mortar_board:", value="70", inline=True)
-        embed.add_field(name="Size (SIZ) :bust_in_silhouette:", value="60", inline=True)
-        embed.add_field(name="Hit Points :heartpulse:", value="12", inline=True)
-        embed.add_field(name="Magic Points :sparkles:", value="10", inline=True)
-        embed.add_field(name="Luck :four_leaf_clover:", value="45", inline=True)
-        embed.add_field(name="Sanity :scales:", value="70", inline=True)
-        
-        await ctx.send(embed=embed)
         
     @commands.command()
     async def CthulhuChangeStats(self, ctx, stat_name, new_value):

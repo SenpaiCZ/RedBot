e5edbb32d0f3168a7838c2d801742d02b6f8e55a
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-21 18:14:46 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -7,14 +7,13 @@ import os
 class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    self.data_file = "player_stats.json"  # Název souboru, kde budou uložena data
+        self.data_file = "player_stats.json"  # Název souboru, kde budou uložena data
 
-    if os.path.exists(self.data_file):
-        with open(self.data_file, "r") as f:
+        if os.path.exists(self.data_file):
+            with open(self.data_file, "r") as f:
                 self.player_stats = json.load(f)
-    else:
-        self.player_stats = {}
+        else:
+            self.player_stats = {}
 
     def save_data(self):
         with open(self.data_file, "w") as f:

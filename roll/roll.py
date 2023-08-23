4b350ee948946ed1dab7dad0d0d9580ea152b43f
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-23 18:56:11 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -989,4 +989,21 @@ class Roll(commands.Cog):
                 await ctx.send("Character creation cancelled.")
         except asyncio.TimeoutError:
             await ctx.send("Character creation timed out.")
+
+
+    @commands.command()
+    async def skillinfo(self, ctx, *, skill_name: str):
+        # Zde můžete definovat informace o dovednostech
+        skills_info = {
+            "Sleight of Hand": "Allows the visual covering-up, secreting, or masking of an object...",
+            "Spot Hidden": "This skill allows the user to spot a secret door or compartment, notice a hidden intruder...",
+            # Přidejte další dovednosti a popisy...
+        }
+        
+        skill_description = skills_info.get(skill_name, "Skill not found.")
+        
+        embed = discord.Embed(title=f"Skill Info: {skill_name}", description=skill_description, color=discord.Color.blue())
+        await ctx.send(embed=embed)
+
+
             

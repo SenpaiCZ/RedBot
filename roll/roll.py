e968e511114602e91c0810002078b6e5b025ea0d
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-23 17:46:41 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -770,3 +770,100 @@ class Roll(commands.Cog):
         )
         
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def cNPC(self, ctx, gender):
+        gender = gender.lower()
+        
+        if gender not in ["male", "female"]:
+            await ctx.send("Invalid gender. Use 'male' or 'female'.")
+            return
+        
+        # Generate random names
+        if gender == "male":
+            first_name_list = [
+                "Aaron", "Abraham", "Addison", "Amos", "Anderson", "Archibald", "August", "Barnabas", "Barney", "Baxter",
+                           "Blair", "Caleb", "Cecil", "Chester", "Clifford", "Clinton", "Cornelius", "Curtis", "Dayton", "Delbert",
+                           "Douglas", "Dudley", "Ernest", "Eldridge", "Elijah", "Emanuel", "Emmet", "Enoch", "Ephraim", "Everett",
+                           "Ezekiel", "Forest", "Gilbert", "Granville", "Gustaf", "Hampton", "Harmon", "Henderson", "Herman",
+                           "Hilliard", "Howard", "Hudson", "Irvin", "Issac", "Jackson", "Jacob", "Jeremiah", "Jonah", "Josiah",
+                           "Kirk", "Larkin", "Leland", "Leopold", "Lloyd", "Luther", "Manford", "Marcellus", "Martin", "Mason",
+                           "Maurice", "Maynard", "Melvin", "Miles", "Milton", "Morgan", "Mortimer", "Moses", "Napoleon", "Nelson",
+                           "Newton", "Noble", "Oliver", "Orson", "Oswald", "Pablo", "Percival", "Porter", "Quincy", "Randall",
+                           "Reginald", "Richmond", "Rodney", "Roscoe", "Rowland", "Rupert", "Sampson", "Sanford", "Sebastian",
+                           "Shelby", "Sidney", "Solomon", "Squire", "Sterling", "Sidney", "Thaddeus", "Walter", "Wilbur", "Wilfred",
+                           "Zadok", "Zebedee"
+            ]
+        else:
+            first_name_list = [
+                "Adelaide", "Agatha", "Agnes", "Albertina", "Almeda", "Amelia", "Anastasia", "Annabelle", "Asenath", "Augusta",
+                             "Barbara", "Bernadette", "Bernice", "Beryl", "Beulah", "Camilla", "Caroline", "Cecilia", "Carmen",
+                             "Charity", "Christina", "Clarissa", "Cordelia", "Cynthia", "Daisy", "Dolores", "Doris", "Edith",
+                             "Edna", "Eloise", "Elouise", "Estelle", "Ethel", "Eudora", "Eugenie", "Eunice", "Florence", "Frieda",
+                             "Genevieve", "Gertrude", "Gladys", "Gretchen", "Hannah", "Henrietta", "Ingrid", "Irene", "Iris",
+                             "Ivy", "Jeanette", "Jezebel", "Josephine", "Joyce", "Juanita", "Keziah", "Laverne", "Leonora", "Loretta",
+                             "Lucretia", "Mabel", "Madeleine", "Margery", "Marguerite", "Marjorie", "Matilda", "Melinda", "Mercedes",
+                             "Mildred", "Millicent", "Muriel", "Myrtle", "Naomi", "Nora", "Octavia", "Ophelia", "Pansy", "Patience",
+                             "Pearle", "Phoebe", "Phyllis", "Rosemary", "Ruby", "Sadie", "Selina", "Selma", "Sibyl", "Sylvia", "Tabitha",
+                             "Ursula", "Veronica", "Violet", "Virginia", "Wanda", "Wilhelmina", "Winifred"
+            ]
+        
+        first_name = random.choice(first_name_list)
+        
+        last_name = random.choice([
+            "Abraham", "Adler", "Ankins", "Avery", "Barnham", "Bentz", "Bessler", "Blakely", "Bleeker", "Bouche",
+                         "Bretz", "Buchman", "Butts", "Caffey", "Click", "Cordova", "Crabtree", "Crankovitch", "Cuthburt",
+                         "Cutting", "Dorman", "Eakley", "Eddie", "Fandrick", "Farwell", "Feigel", "Fenske", "Fillman",
+                         "Finley", "Firske", "Flanagan", "Franklin", "Freeman", "Frisbe", "Gore", "Greenwald", "Hahn",
+                         "Hammermeister", "Heminger", "Hogue", "Hollister", "Kasper", "Kisro", "Kleeman", "Lake", "Levard",
+                         "Lockhart", "Luckstrim", "Lynch", "Mantei", "Marsh", "McBurney", "McCarney", "Moses", "Nickels",
+                         "O'Neil", "Olson", "Ozanich", "Patterson", "Patzer", "Peppin", "Porter", "Posch", "Raslo", "Razner",
+                         "Rifenberg", "Riley", "Ripley", "Rossini", "Schiltgan", "Schmidt", "Schroeder", "Schwartz", "Shane",
+                         "Shattuck", "Shea", "Slaughter", "Smith", "Speltzer", "Stimac", "Stimac","Strenburg","Strong","Swanson",
+                        "Tillinghast","Traver","Urton","Vallier","Wagner","Walsted","Wang","Warner","Webber","Welch","Winters","Yarbrough","Yeske"
+        ])  # ... všechna příjmení
+        
+        full_name = first_name
+        
+        # 50% chance for a second first name
+        if random.random() < 0.5:
+            second_first_name = random.choice(first_name_list)
+            full_name = f"{first_name} {second_first_name}"
+        
+        full_name += f" {last_name}"
+        
+        # Generate stats
+        STR = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        CON = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        SIZ = 5 * (sum(sorted([random.randint(1, 6) for _ in range(2)])) + 6)
+        DEX = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        APP = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        INT = 5 * (sum(sorted([random.randint(1, 6) for _ in range(2)])) + 6)
+        POW = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        EDU = 5 * (sum(sorted([random.randint(1, 6) for _ in range(2)])) + 6)
+        LUCK = 5 * sum(sorted([random.randint(1, 6) for _ in range(3)])[1:])
+        HP = (CON + SIZ) // 10
+        
+        stats = {
+            "STR": ":muscle:",
+            "DEX": ":runner:",
+            "CON": ":heart:",
+            "INT": ":brain:",
+            "POW": ":zap:",
+            "CHA": ":sparkles:",
+            "EDU": ":mortar_board:",
+            "SIZ": ":bust_in_silhouette:",
+            "HP": ":heartpulse:",
+            "LUCK": ":four_leaf_clover:",
+        }
+        
+        stats_embed = "\n".join([f"{emoji} {stat}: {value}" for stat, value in stats.items()])
+        
+        embed = discord.Embed(
+            title="NPC Character Sheet",
+            description=f":game_die: **Name:** {full_name}\n\n{stats_embed}",
+            color=discord.Color.gold()
+        )
+        
+        await ctx.send(embed=embed)
+

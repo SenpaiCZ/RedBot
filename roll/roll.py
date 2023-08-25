6eced55d50a472f59284db5146c2053ffc8960fb
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-25 11:54:15 +0200

    Update roll.py

--- roll/roll.py
+++ roll/roll.py
@@ -8,7 +8,8 @@ import asyncio
 class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.data_file = "/home/pi/.local/share/SenpaiBot/cogs/RepoManager/repos/senpaicz/roll/player_stats.json" 
+        self.data_file = "player_stats.json" 
+        #Ex-cesta /home/pi/.local/share/SenpaiBot/cogs/RepoManager/repos/senpaicz/roll/
 
         if os.path.exists(self.data_file):
             with open(self.data_file, "r") as f:

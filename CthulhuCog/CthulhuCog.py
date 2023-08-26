136043816ccf2d7d18ee7a225bb49c7ca44f6654
SenpaiCZ <stuffyskiller@gmail.com>
2023-08-26 13:31:55 +0200

    Update CthulhuCog.py

--- CthulhuCog/CthulhuCog.py
+++ CthulhuCog/CthulhuCog.py
@@ -8,7 +8,7 @@ import asyncio
 class CallofCthulhuCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)
+        self.config = Config.get_conf(self, identifier=654321987)
         default_guild = {
             "player_stats": {}
         }
